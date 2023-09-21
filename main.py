8079cf9ed5329961bc1eb6d7240c37ed4cb1c2a8
Aidar Asankadyrov <dspace438@gmail.com>
2023-09-21 13:39:47 +0600

    made two tasks

---
+++ main.py
@@ -0,0 +1,30 @@
+###PROBLEM1
+#	a = [1,2,2,1,6,4,7,8,6,5]
+#b=[2,6,7,8]
+
+#PROBLEM2
+#PROBLEM3
+#PROBLEM4
+#PROBLEM5
+#a=input("enter login: ")
+#b=int(input("Password: "))
+#if a==True
+#	print("True")
+#elif b==True:
+#	print("True")
+#else:
+#	print("False")
+#PROBLEM6
+#a=input("Type your genre: ")
+#x=int(input("Type your time: "))
+#if 21>=x:
+#	print("costs 500")
+#else:
+#	print("300")
+#PROBLEM7
+#district=input("Choose your district: ")
+#if a
+#PROBLEM8
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+
